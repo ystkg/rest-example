2d8eb210ea05ca4138ba1e0bd8edf6d0b7cc77bc
ystkg <134891927+ystkg@users.noreply.github.com>
2024-08-09 20:14:30 +0900

    リファクタリング

--- handler/server.go
+++ handler/server.go
@@ -10,21 +10,24 @@ func NewEcho(h *Handler) *echo.Echo {
 	e := echo.New()
 
 	e.Validator = NewCustomValidator()
+
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	e.Use(timeout(h.timeoutSec))
 
-	e.POST("/user", h.CreateUser)
-	e.POST("/user/:name/token", h.GenToken)
+	root := e.Group("/")
+	root.Use(timeout(h.timeoutSec))
 
-	g := e.Group("/v1")
+	root.POST("user", h.CreateUser)
+	root.POST("user/:name/token", h.GenToken)
+
+	g := root.Group("v1/")
 	g.Use(echojwt.WithConfig(h.newJwtConfig()))
 
-	g.POST("/price", h.CreatePrice)
-	g.GET("/price", h.FindPrices)
-	g.GET("/price/:id", h.FindPrice)
-	g.PUT("/price/:id", h.UpdatePrice)
-	g.DELETE("/price/:id", h.DeletePrice)
+	g.POST("price", h.CreatePrice)
+	g.GET("price", h.FindPrices)
+	g.GET("price/:id", h.FindPrice)
+	g.PUT("price/:id", h.UpdatePrice)
+	g.DELETE("price/:id", h.DeletePrice)
 
 	return e
 }
